60567ee25c3da220b890322f3067f6b6e74c158e
Louis Savoie <thelousavoie@gmail.com>
2020-08-24 16:28:59 -0400

    add new item form

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ let app = new Vue({
     el: '#app',
     data: {
         title: 'Fridge List',
-        list: ["Beef", "Carrots", "Bay Leaves"]
+        list: ["Beef", "Carrots", "Bay Leaves"],
+        newItem: null,
+        error: null
     },
     methods: {
         add(item) {
-            this.list.push(item);
+            if (this.newItem) {
+                this.list.push(item);
+                this.newItem = null;
+                this.error = null;
+            }
+            else {
+                this.error = "New Item is empty!";
+            }
         },
         remove(item) {
             let index = this.list.indexOf(item);
