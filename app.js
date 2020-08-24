07a99d7b7e861c35812ab10ef3bb379acb9eb94b
Louis Savoie <thelousavoie@gmail.com>
2020-08-24 15:18:29 -0400

    add a list

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 let app = new Vue({
     el: '#app',
     data: {
-        title: 'Fridge List'
+        title: 'Fridge List',
+        list: ["Beef", "Carrots", "Bay Leaves"]
+    },
+    methods: {
+        add(item) {
+            this.list.push(item);
+        },
+        remove(item) {
+            let index = this.list.indexOf(item);
+            this.list.splice(index, 1);
+        }
     }
 });
