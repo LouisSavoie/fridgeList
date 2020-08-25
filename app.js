b6f516694a3e88e318e55a2f8440836e9fa30d1c
Louis Savoie <thelousavoie@gmail.com>
2020-08-24 20:43:37 -0400

    move list to compnent, add states

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        title: 'Fridge List',
-        list: ["Beef", "Carrots", "Bay Leaves"],
-        newItem: null,
-        error: null
+Vue.component('fridge', {
+    template: `
+        <div>
+            <h2>Fridge</h2>
+            <form v-on:submit.prevent="onSubmit" v-on:submit="add(newItem)">
+                <p>{{ error }}</p>
+                <input type="text" v-model="newItem" placeholder="New Item">
+                <!-- <button type="button" v-on:click="add(newItem)">Add</button> -->
+            </form>
+            <ul>
+                <li v-for="item in list">
+                    <span>{{ item }}</span>
+                    <span v-on:click="remove(item)">    X</span>
+                </li>
+            </ul>
+        </div>
+    `,
+    data() {
+        return {
+            list: ["Beef", "Carrots", "Bay Leaves"],
+            newItem: null,
+            error: null
+        }
     },
     methods: {
         add(item) {
@@ -20,6 +36,19 @@ let app = new Vue({
         remove(item) {
             let index = this.list.indexOf(item);
             this.list.splice(index, 1);
+        }
+    }
+});
+
+let app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Fridge List',
+        state: 0
+    },
+    methods: {
+        setState(index) {
+            this.state = index;
         }
     }
 });
